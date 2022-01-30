76a1bd1c0ae6e5e07403940cd2a556861ec7dd14
Rushi Raval <rushiraval0@gmail.com>
2022-01-30 15:50:33 +0530

    updated login and register activity

--- app/src/main/java/com/example/hybridclass/LoginActivity.java
+++ app/src/main/java/com/example/hybridclass/LoginActivity.java
@@ -1,15 +1,95 @@
 package com.example.hybridclass;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.regex.Pattern;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText email;
+    private EditText password;
+    private Button signIn;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        email=(EditText) findViewById(R.id.etEmail);
+        password=(EditText) findViewById(R.id.etPassword);
+        signIn=(Button) findViewById(R.id.btLogin);
+        mAuth= FirebaseAuth.getInstance();
+
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                logIn();
+            }
+        });
+
+    }
+
+    private void logIn(){
+        String _email=email.getText().toString().trim();
+        String _password=password.getText().toString().trim();
+
+        if(_email.isEmpty())
+        {
+            email.setError("Email is required");
+            email.requestFocus();
+            return;
+        }
+        if (!Patterns.EMAIL_ADDRESS.matcher(_email).matches()) {
+            email.setError("Email is invalid");
+            email.requestFocus();
+            return;
+        }
+        if(_password.isEmpty())
+        {
+            password.setError("Password is required");
+            password.requestFocus();
+            return;
+        }
+        if(_password.length()<6)
+        {
+            password.setError("Password < 6");
+            password.requestFocus();
+            return;
+        }
+
+        mAuth.signInWithEmailAndPassword(_email,_password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful())
+                {
+                    Toast.makeText(LoginActivity.this,"Successfully Logged In",Toast.LENGTH_LONG).show();
+                }
+                else
+                {
+                    Toast.makeText(LoginActivity.this,"Error Logging In",Toast.LENGTH_LONG).show();
+                }
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(LoginActivity.this,"Error "+e.getMessage(),Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/hybridclass/RegisterActivity.java
+++ app/src/main/java/com/example/hybridclass/RegisterActivity.java
@@ -1,5 +1,6 @@
 package com.example.hybridclass;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -10,8 +11,16 @@ import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class RegisterActivity extends AppCompatActivity {
 
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +30,7 @@ public class RegisterActivity extends AppCompatActivity {
         final EditText password = findViewById(R.id.etPassword);
         final EditText confirmPassword = findViewById(R.id.etRePassword);
         final RadioGroup role = findViewById(R.id.rg);
+        mAuth = FirebaseAuth.getInstance();
 
         final Button btn = findViewById(R.id.btReg);
         DAOUser dao = new DAOUser();
@@ -30,11 +40,50 @@ public class RegisterActivity extends AppCompatActivity {
             int selectedId = role.getCheckedRadioButtonId();
             RadioButton radioButton = (RadioButton) findViewById(selectedId);
 
-            User user = new User(email.getText().toString(),name.getText().toString(),password.getText().toString(),confirmPassword.getText().toString(),radioButton.getText().toString());
-            dao.add(user).addOnSuccessListener(suc-> {
-                Toast.makeText(this, "Successful", Toast.LENGTH_SHORT).show();
-            }).addOnFailureListener(er->{
-                Toast.makeText(this, ""+er.getMessage(), Toast.LENGTH_SHORT).show();
+
+//            dao.add(user).addOnSuccessListener(suc-> {
+//                Toast.makeText(this, "Successful", Toast.LENGTH_SHORT).show();
+//            }).addOnFailureListener(er->{
+//                Toast.makeText(this, ""+er.getMessage(), Toast.LENGTH_SHORT).show();
+//            });
+
+            String _email = email.getText().toString();
+            String _password = password.getText().toString();
+            System.out.println("Successful");
+
+            mAuth.createUserWithEmailAndPassword(_email,_password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(task.isSuccessful())
+                    {
+                        System.out.println("Successful");
+                        User user = new User(email.getText().toString(),name.getText().toString(),password.getText().toString(),confirmPassword.getText().toString(),radioButton.getText().toString());
+
+                        FirebaseDatabase.getInstance().getReference(User.class.getSimpleName())
+                                .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                                .setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if(task.isSuccessful())
+                                {
+                                    Toast.makeText(RegisterActivity.this,"Successful",Toast.LENGTH_LONG).show();
+                                }
+                                else {
+                                    Toast.makeText(RegisterActivity.this,"Failed.",Toast.LENGTH_LONG).show();
+                                }
+                            }
+                        });
+
+                    }
+                    else {
+                            Toast.makeText(RegisterActivity.this,"Failed.",Toast.LENGTH_LONG).show();
+                    }
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(RegisterActivity.this,"Failed."+e.getMessage(),Toast.LENGTH_LONG).show();
+                }
             });
         } );
 
