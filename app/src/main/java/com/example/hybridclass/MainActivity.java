c3b4dae2c5580f36963ec1f5b5215629556aaaa2
Rushi Raval <rushiraval0@gmail.com>
2022-02-04 18:02:52 +0530

    added splashscreen

--- app/src/main/java/com/example/hybridclass/MainActivity.java
+++ app/src/main/java/com/example/hybridclass/MainActivity.java
@@ -4,39 +4,97 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button loginBtn;
-    Button registerBtn;
+    ImageView ivTop,ivBottom;
+    TextView textView;
+    CharSequence charSequence;
+    int index;
+    long delay=200;
+    Handler handler = new Handler();
+    FirebaseAuth firebaseAuth;
+    FirebaseUser firebaseUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        loginBtn = findViewById(R.id.loginButton);
-        registerBtn = findViewById(R.id.registerButton);
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseUser = firebaseAuth.getCurrentUser();
+        ivTop = findViewById(R.id.iv_top);
+        ivBottom = findViewById(R.id.iv_bottom);
 
-        loginBtn.setOnClickListener(new View.OnClickListener() {
+        textView = findViewById(R.id.appName);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        Animation animation1 = AnimationUtils.loadAnimation(this,R.anim.top_wave);
+        ivTop.setAnimation(animation1);
+
+
+        animateText("HybridClass");
+
+//        Glide.with(this).load("https://firebasestorage.googleapis.com/v0/b/demoapp-ae96a.appspot.com/o/heart_beat.gif?alt=media&token=b21dddd8-782c-457c-babd-f2e922ba172b")
+//                .asGif()
+//                .diskCacheStrategy(DiskCacheStrategy.ALL)
+//                .into(iv)
+
+        Animation animation2 = AnimationUtils.loadAnimation(this,R.anim.bottom_wave);
+
+        ivBottom.setAnimation(animation2);
+
+        new Handler().postDelayed(new Runnable() {
             @Override
-            public void onClick(View v) {
+            public void run() {
+                if(firebaseUser != null)
+                {
+                    startActivity(new Intent(MainActivity.this, DashboardActivity.class).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
 
-                Intent login =new Intent(MainActivity.this,LoginActivity.class);
-                startActivity(login);
+                }
+                else {
+                    startActivity(new Intent(MainActivity.this, LoginActivity.class).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
+                }
+                finish();
             }
-        });
-
-        registerBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                Intent register =new Intent(MainActivity.this,RegisterActivity.class);
-                startActivity(register);
-            }
-        });
+        },4000);
 
     }
+    Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            textView.setText(charSequence.subSequence(0, index++));
+            if (index <= charSequence.length()) {
+                handler.postDelayed(runnable, delay);
+            }
+        }
+    };
+
+    public void animateText(CharSequence cs)
+    {
+        charSequence= cs;
+
+        index = 0;
+
+        textView.setText("");
+
+        handler.removeCallbacks(runnable);
+        handler.postDelayed(runnable,delay);
+
+    }
+
 }
